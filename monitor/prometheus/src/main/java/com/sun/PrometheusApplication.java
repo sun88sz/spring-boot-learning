f7d8f87a653d917e15fe01a1fddd3aa5b4b5ae0b
孙晨杰 <sunchenjie@gcb365.com>
2019-12-19 15:01:09 +0800

    prometheus

---
+++ monitor/prometheus/src/main/java/com/sun/PrometheusApplication.java
@@ -0,0 +1,25 @@
+package com.sun;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author : Sun
+ * @date : 2018/8/1 11:34
+ */
+@SpringBootApplication
+public class PrometheusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusApplication.class, args);
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
+}
